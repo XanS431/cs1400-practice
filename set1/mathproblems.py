c6a1a0d44fbd6ccdfbff72c1aef3ab773a4e6e20
XanS431 <26stephxanc@washk12.org>
2025-01-15 08:51:47 -0700

    Add files via upload

---
+++ set1/mathproblems.py
@@ -0,0 +1,5 @@
+print ((5+5) == (20-10))
+print((20/5+3) >= 4)
+print(3 > (21/7))
+print(True==1)
+print(False==0)
---
+++ set1/my_first_function.py
@@ -0,0 +1,5 @@
+my_first_function:
+print("Hello, World!")
+print("I am programming.")
+def my_first_function():
+print("Goodbye.")
