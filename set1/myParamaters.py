ec7b340d905ccbba8859d5d64b072f3b2f4e5d45
XanS431 <26stephxanc@washk12.org>
2025-01-24 08:48:29 -0700

    Add files via upload

---
+++ set1/myParamaters.py
@@ -0,0 +1,9 @@
+print(38 + 92) # should return 130
+
+product = print(38 * 92) # should return 3496
+print(product)
+
+d = 2
+r = 24
+at_least_double = print(r * d) # should return 2 * r
+print(at_least_double)
