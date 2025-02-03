800f2c26c8ed1d13d0c39bb24388158a44a0678c
XanS431 <26stephxanc@washk12.org>
2025-02-03 09:18:08 -0700

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,26 @@
+def myname():
+    print("Xander")
+
+myname()
+
+def lovebirds():
+    print("Toa" , "Carter")
+    print("Toa loves Carter")
+
+lovebirds()
+
+def password_check1():
+    Password = print(input("Password, please?"))
+    if Password == "princess89":
+        print("True! Access Granted")
+
+password_check1()
+
+def password_check2():
+    password = print(input("Password, please?"))
+    if password == "rocky1977":
+        print("True! Access Geanted")
+    else:
+        print("False! Access Denied")
+
+password_check2()
