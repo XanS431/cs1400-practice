1eadb88bc47667466a2327063c494b919dadbe37
XanS431 <26stephxanc@washk12.org>
2025-03-19 09:02:05 -0600

    Add files via upload

---
+++ Adjacent items in lists and strings.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'grape', 'orange', 'berry']
+print(fruits[3]) 
+for i in range(len(fruits)):
+    print(fruits[i])
+
+
+call = (["rock","hard place","pillow","hard place","feather","rock","hard place","rock"])
+
+stuck = (["stuck"])
+
+print(call + stuck)
